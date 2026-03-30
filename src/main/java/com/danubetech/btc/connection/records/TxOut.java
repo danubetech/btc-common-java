fafafdd218148895cd6fb9317586b75daf1db02a
Markus Sabadello <markus@danubetech.com>
2026-03-30 22:59:00 +0200

    Convenience method

--- src/main/java/com/danubetech/btc/connection/records/TxOut.java
+++ src/main/java/com/danubetech/btc/connection/records/TxOut.java
@@ -1,5 +1,8 @@
 package com.danubetech.btc.connection.records;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 public record TxOut(
         String txId,
         String scriptPubKey,
@@ -10,5 +13,13 @@ public record TxOut(
 
     public TxOut txId(String txId) {
         return new TxOut(txId, scriptPubKey, scriptPubKeyAsm, scriptPubKeyType, scriptPubKeyAddress, value);
+    }
+
+    public byte[] scriptBytes() {
+        try {
+            return Hex.decodeHex(scriptPubKey);
+        } catch (DecoderException ex) {
+            throw new IllegalStateException("Cannot hex-decode script: " + scriptPubKey + ": " + ex.getMessage(), ex);
+        }
     }
 }
